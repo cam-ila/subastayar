dfdc94e1588647be9862c150ea0721e83b5526d6
Andres Cito Bernardi <cito.bernardi@gmail.com>
2015-04-24 20:46:25 -0300

    updates bid and user seeders

--- app/database/seeds/BidTableSeeder.php
+++ app/database/seeds/BidTableSeeder.php
@@ -8,17 +8,43 @@ class BidTableSeeder extends Seeder {
   public function run()
   {
     $ropa = App\Category::whereName('Ropa y Accesorios')->first();
+    $antig = App\Category::whereName('Antigüedades')->first();
+    $animales = App\Category::whereName('Animales')->first();
+    $otros = App\Category::whereName('Otros')->first();
+    $alimentos = App\Category::whereName('Alimentos')->first();
 
-    App\User::whereEmail('ramirolamas@ramiro.com')->firstOrFail()->bids()->saveMany([
+
+    App\User::whereEmail('carlosmaidana@carlos.com')->firstOrFail()->bids()->saveMany([
       new App\Bid([
-        'title' => 'Guantes de acero',
+        'title' => 'Llama',
         'description' => 'Guante de malla de acero inox. tejido, anticorte, marca *manulatex* de industria francesa',
         'category_id' => $ropa->id,
       ]),
       new App\Bid([
         'title' => 'Espejo',
         'description' => 'Espejo sin marco. Medidas: 0.8m x 1.2m',
-        'category_id' => $ropa->id,
+        'category_id' => $antig->id,
+      ])
+    ]);
+
+    App\User::whereEmail('catalinaperez@catalina.com')->firstOrFail()->bids()->saveMany([
+      new App\Bid([
+        'title' => 'Llama',
+        'description' => 'Llama adulta oriunda de Tilcara. Es mansita',
+        'category_id' => $animales->id,
+      ]),
+     new App\Bid([
+        'title' => 'Kriptonita',
+        'description' => '200 gramos de Kriptonita',
+        'category_id' => $otros->id,
+      ])
+    ]);
+
+    App\User::whereEmail('sergioramirez@sergioramirez.com')->firstOrFail()->bids()->saveMany([
+      new App\Bid([
+        'title' => 'Aceite y Vinagre',
+        'description' => '200ml de aceite y 300ml de vinagre. No incluye fascos',
+        'category_id' => $alimentos->id,
       ])
     ]);
   }
