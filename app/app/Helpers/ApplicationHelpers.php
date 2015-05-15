8bc9d4d9e2c48843caea42d3dfdf48867aebe03a
Ivan Karl <ivan6258@gmail.com>
2015-05-14 22:11:33 -0300

    adds various view helpers

--- app/app/Helpers/ApplicationHelpers.php
+++ app/app/Helpers/ApplicationHelpers.php
@@ -19,3 +19,16 @@ function pluralize($key, $file = 'models')
   return t($file.'.'.$key, 2);
 }
 
+function destroy_link($resource) {
+  return link_to(route(str_plural($resource->model()) . '.edit', $resource), 'Editar', ['class' => 'btn btn-default']) .
+    Form::open(['url' => route(str_plural($resource->model()) .'.destroy', $resource), 'class' => 'destroy-link']) .
+    Form::input('hidden', '_method', 'DELETE') .
+    Form::submit('Borrar', ['class' => 'btn btn-danger btn-destroy']) .
+    Form::close() ;
+}
+
+function create_link($model) {
+  return link_to(route(str_plural($model) . '.create'),
+    trans('crud.titles.create',
+    ['model' => translate($model)]), ['class' => 'btn btn-success']);
+}
