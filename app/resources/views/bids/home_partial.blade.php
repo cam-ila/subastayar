48c2bd915ebcd41af34d640b564687318f65e9c9
Ivan Karl <ivan6258@gmail.com>
2015-05-20 00:24:04 -0300

    shows pictures in home view

--- app/resources/views/bids/home_partial.blade.php
+++ app/resources/views/bids/home_partial.blade.php
@@ -1,7 +1,7 @@
 <div class="bid">
   <div class="img-wrapper">
     <a href="{{ route('bids.show', $bid)}}">
-      {!! HTML::image($bid->image, $bid->title, ['class' => 'img-rounded'])  !!}
+    {!! HTML::image('/uploads/img/' . $resource->image, $resource->title, ['class' => 'img-rounded img-responsive img-thumbnail']) !!}
     </a>
   </div>
   <div class="description">
