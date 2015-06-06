1fad8611e3487fc697574029d933c839ffa40238
Camila Bignone <camilabignone@gmail.com>
2015-06-05 21:37:09 -0300

    improves search partials

--- app/resources/views/shared/partials/search.blade.php
+++ app/resources/views/shared/partials/search.blade.php
@@ -1,4 +1,8 @@
-{!! Form::open(['url' => route(str_plural($model).'.index'), 'method' => 'GET', 'class' => (isset($navbar) ? 'navbar-form navbar-left' : 'panel-form'), 'role' => 'search']) !!}
+{!! Form::open([
+	'url' => (isset($url) ? $url : route(str_plural($model).'.index')),
+ 'method' => 'GET',
+  'class' => (isset($navbar) ? 'navbar-form navbar-left' : 'panel-form'),
+   'role' => 'search']) !!}
 {!! Form::input('hidden', 'model', $model) !!}
 <div class="input-group">
   {!! Form::input('search', 'query', (isset($query) ? $query : ''), ['placeholder' => 'Buscar '.translate($model).' ...', 'class' => 'form-control'])  !!}
