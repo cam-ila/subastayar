df319e9828a33f633b2e628e53baee0e3cda3e6b
Ivan Karl <ivan6258@gmail.com>
2015-06-06 02:32:34 -0300

    adds category filter

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -1,11 +1,15 @@
 $(function() {
-  // menu
   $('.btn-destroy').on('click', function(event){
     event.preventDefault();
-    if (confirm('Esta seguro que desea realizar esta accion?')){
-      $(this).closest('form').submit();
-    }
+    if (confirm('Esta seguro que desea realizar esta accion?')){ $(this).closest('form').submit() }
   });
+
   $(".sortable-table").stupidtable();
-  $('[data-toggle="tooltip"]').tooltip()
+
+  $('[data-toggle="tooltip"]').tooltip();
+
+  $(document).on('change', '.js-filter-form', function() {
+    window.location = $(this).data('url') + $('.js-filter-form option:selected').val();
+  });
+
 });
--- app/resources/views/filters/category.blade.php
+++ app/resources/views/filters/category.blade.php
@@ -1,3 +1,6 @@
-{!! Form::open(['url' => route('home'), 'method' => 'GET', 'class' => 'navbar-form navbar-left', 'role' => 'search', 'data-toggle' => 'tooltip', 'title' => trans('filters.by.category'), 'data-placement' => 'bottom']) !!}
-  {!! Form::select('category_id', App\Models\Category::query()->lists('name', 'id'), null, ['class' => 'form-control', 'id' => 'category']) !!}
-{!! Form::close() !!}
+<div class="js-filter-form navbar-form navbar-left" data-url="{{ route('bidsByCategory', null) }}/">
+{!! Form::select('category_id', App\Models\Category::query()->lists('name', 'id'), null, [
+      'class' => 'form-control',
+      'id'    => 'category' ])
+!!}
+</div>
