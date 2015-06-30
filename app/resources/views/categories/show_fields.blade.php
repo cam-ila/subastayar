04824ecd44868c8eaab27241a6b46f848821e743
Ivan Karl <ivan6258@gmail.com>
2015-06-30 15:55:55 -0300

    removes categories link

--- app/resources/views/categories/show_fields.blade.php
+++ app/resources/views/categories/show_fields.blade.php
@@ -2,8 +2,4 @@
   <div class="row">
     <div class="h4">{{ $resource }}</div>
   </div>
-  <div class="row">
-    Esta categoria tiene
-    {!! link_to(route('bidsByCategory', $resource), $resource->bids->count() . ' ' .  t('models.bid', $resource->bids->count()) ) !!}.
-  </div>
 </div>
