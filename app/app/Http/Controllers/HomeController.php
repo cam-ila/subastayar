3fd323e87e074628476bb2756c93c98fb377f619
Ivan Karl <ivan6258@gmail.com>
2015-07-10 20:42:11 -0300

    fixes categories index

--- app/app/Http/Controllers/HomeController.php
+++ app/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use App\Models\Bid as Bid;
 use App\Models\User as User;
+use App\Models\Category as Category;
 
 class HomeController extends Controller {
 
@@ -19,11 +20,11 @@ class HomeController extends Controller {
     return view('home.bid', compact('resource'));
   }
 
-  public function bidsByCategory($category_id, Request $request)
+  public function bidsByCategory(Category $category, Request $request)
   {
     $query     = $request->input('query');
     $model     = 'bid';
-    $resources = Bid::whereCategoryId($category_id)->get();
+    $resources = Bid::active()->whereCategoryId($category->id)->get();
     return view('home.index', compact('resources', 'query', 'model', 'category_id'));
   }
 
