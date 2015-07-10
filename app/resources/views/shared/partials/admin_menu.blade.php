82df711bdad7b8bfd4d96a600ffe88a17fb47f78
Ivan Karl <ivan6258@gmail.com>
2015-07-10 20:03:36 -0300

    fixes nav links

--- app/resources/views/shared/partials/admin_menu.blade.php
+++ app/resources/views/shared/partials/admin_menu.blade.php
@@ -1,3 +1,2 @@
 <li>{!! link_to(route('categories.index'), 'Administrar Categorias') !!}</li>
 <li>{!! link_to(route('users.admin'), 'Asignar administrador') !!}</li>
-<li>{!! link_to(route('sales.between_dates'), 'Listar Ventas') !!}</li>
